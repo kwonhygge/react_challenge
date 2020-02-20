1b939307fc86eb6d637b6ab675c8bfc08a40e6bc
kwonhygge <kitteniso1103@gmail.com>
2020-02-20 22:41:14 +0900

    Challenge Done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote){
+    setNotes(prevnotes => {
+      return [...prevnotes,newNote];
+    })
+  }
+
+
+  function deleteNote(id){
+    setNotes(prevNotes=>{
+      return notes.filter((item,index)=>{
+        return index !== id;
+      })
+    })
+  }
+
+  console.log(notes);
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAdd={addNote}/>
+      {notes.map((eachNote,index)=>(
+        <Note key={index} id={index} title={eachNote.title} content={eachNote.content} onDelete={deleteNote} />
+      )   
+      )} 
       <Footer />
     </div>
   );
